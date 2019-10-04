0d03a6b93fa59072b902c0e794bf93d527b15f78
Nicolaos Derr <nderr@Nicolaoss-MacBook-Pro.local>
2019-10-03 23:36:59 -0600

    Created new UA

---
+++ js/app.js
@@ -0,0 +1,14 @@
+class enemies{
+	constructor(name){
+		this.name = name;
+	}
+
+}
+
+class potion{
+	constructor(name, health, mana){
+		this.name = name;
+		this.health = health;
+		this.mana = mana; 
+	}
+}
