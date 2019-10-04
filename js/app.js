46ca8afd3b9d049eb0829989f6e15e7609f0d054
Nicolaos Derr <nderr@Nicolaoss-MacBook-Pro.local>
2019-10-04 13:14:26 -0600

    Fixed log

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+const $clearLogButton = $('#clearlog');
+const $update = $('#updates'); 
+
+
+
 class enemies{
 	constructor(name){
 		this.name = name;
@@ -12,3 +17,20 @@ class potion{
 		this.mana = mana; 
 	}
 }
+
+const player = {
+	maxHP: 20,
+	maxMP: 8,
+	currentHP: 20,
+	currentMP: 8,
+	strength: 3,
+	defense: 3,
+	attack(){
+		$($update).prepend('<p>You attack the enemy!</p>');
+	}
+}
+
+$($clearLogButton).on('click', (e) => {
+	console.log('clear');
+	$($update).empty();
+});
