692cc547b8866efc82b0003302617ebf641d1ade
Nicolaos Derr <nderr@Nicolaoss-MacBook-Pro.local>
2019-10-07 16:20:23 -0600

    made some css/html edits. Added base for enemy adder

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 //Global varibales being used
-const $clearLogButton = $('#clearlog');
-const $update = $('#updates'); 
-const $upperLeftButton = $('#upperleft-button');
-const $upperRightButton = $('#upperright-button');
-const $lowerLeftButton = $('#lowerleft-button');
-const $lowerRightButton = $('#lowerright-button');
-const $commandDescription = $('#commandDescription');
-let magicToggle = false;
-let itemToggle = false;
+	const $clearLogButton = $('#clearlog');
+	const $update = $('#updates'); 
+	const $upperLeftButton = $('#upperleft-button');
+	const $upperRightButton = $('#upperright-button');
+	const $lowerLeftButton = $('#lowerleft-button');
+	const $lowerRightButton = $('#lowerright-button');
+	const $commandDescription = $('#commandDescription');
+	let magicToggle = false;
+	let itemToggle = false;
 
 //Commands for upper left button 
 	//Attack or fire
 	$($upperLeftButton).on('click', () => {
 		if(magicToggle === true && itemToggle === false){
 			$($update).prepend('<p>You used the fire spell</p>');
-			//fire function
+			player.fireSpell();
 		}
 		if(magicToggle === false && itemToggle === false){
 			$($update).prepend('<p>You attack the enemy</p>');
@@ -33,7 +33,7 @@ let itemToggle = false;
 			$($commandDescription).text('Use fire to attack the enemy - 25% chance for burn (-1hp per turn for 2-5 turns)');
 		}
 		}, function(){
-			$($commandDescription).text('-');
+			$($commandDescription).text('');
 		}
 	);
 
@@ -67,7 +67,7 @@ let itemToggle = false;
 			$($commandDescription).text(`Restore 50% of max health (${Math.ceil(player.maxHP/2)} points)`);
 		}
 		}, function(){
-			$($commandDescription).text('-');
+			$($commandDescription).text('');
 		}
 	);
 
@@ -114,7 +114,7 @@ let itemToggle = false;
 			$($commandDescription).text('Use ice attack - 25% chance of frostbite (enemy atk reduced by 10% for 2-5 turns)');
 		}
 		}, function(){
-			$($commandDescription).text('-');
+			$($commandDescription).text('');
 		}
 	);
 
@@ -158,121 +158,149 @@ let itemToggle = false;
 			$($commandDescription).text(`Restore 50% of max MP (${Math.ceil(player.maxMP/2)} points)`);
 		}
 		}, function(){
-			$($commandDescription).text('-');
+			$($commandDescription).text('');
 		}
 	);
 
 
-class enemies{
-	constructor(name, strength, attack, hp, defense, fireWeakness, iceWeakness, lightningWeakness, imageURL, exp){
-		this.name = name;
-		this.strength = strength;
-		this.attack = attack;
-		this.HP = hp;
-		this.defense = defense
-		this.fireWeakness = fireWeakness;
-		this.iceWeakness = iceWeakness;
-		this.lightningWeakness = lightningWeakness;
-		this.burn = false;
-		this.frostbite = false;
-		this.shock = false;
-		this.imageURL = imageURL;
-		this.expPts = exp;
-	}
-	attack(){
-		$($update).prepend(`<p>The enemy attacks you!</p>`);
-	}
-}
-
-const bat = new enemies('Bat', 2, "bite", 10, 2, false, false, false, 'url(../images/enemies/bat.gif)', 2);
-
-const healthPotion = {
-	name: 'Health Potion',
-	recovery: 0.5,
-};
-
-const manaPotion = {
-	name: 'Mana Potion',
-	recovery: 0.5,
-}
-
-
-const player = {
-	maxHP: 20,
-	maxMP: 8,
-	currentHP: 20,
-	currentMP: 8,
-	level: 1,
-	strength: 3,
-	defense: 3,
-	currentEXP: 0,
-	levelUpEXP: 5,
-	healthPotions: 3,
-	manaPotions: 3,
-	playerTurn(){
-		$($update).prepend('<p>You attack the enemy!</p>');
-	},
-	levelUp(){
-		$($update).prepend('<p>You have leveled up!</p>');
-		player.level++;
-		player.maxHP +=3;
-		player.maxMP +=2;
-		player.currentHP = player.maxHP;
-		player.currentMP = player.maxMP;
-		player.strength++;
-		player.defense++;
-		player.levelUpEXP += Math.floor(5*0.75*player.level);
-		$($update).prepend(`<p>Max HP = ${player.maxHP}</p>`);
-		$($update).prepend(`<p>Max MP = ${player.maxMP}</p>`);
-		$($update).prepend(`<p>Strength = ${player.strength}</p>`);
-		$($update).prepend(`<p>Defense = ${player.defense}</p>`);
-		$($update).prepend(`<p>EXP to next level: ${player.levelUpEXP - player.currentEXP}</p>`);
-	},
-	fireSpell(){
-
-	},
-	iceSpell(){
-
-	},
-	lightningSpell(){
-
-	},
-	healthPotion(){
-		if(this.healthPotions > 0 && this.currentHP < this.maxHP){
-			if(Math.ceil(healthPotion.recovery * this.maxHP) + this.currentHP >= this.maxHP){
-				this.currentHP = this.maxHP;
-			} else{
-				this.currentHP += Math.ceil(healthPotion.recovery * this.maxHP);
-			}
-			console.log(player.currentHP);
-			$('#currentHP').text(player.currentHP);
-			$('#hpBar').css('width', `${(this.currentHP/this.maxHP)*100}%`);
-			this.healthPotions--;
-		} else{
-			if(this.healthPotions = 0){
-				$($update).prepend('<p>You have no health potions left</p>')
-			} else{
-				$($update).prepend('<p>Health is already at max</p>');
-			}
+//Enemies
+	class enemies{
+		constructor(name, strength, attack, hp, defense, fireWeakness, iceWeakness, lightningWeakness, imageURL, exp){
+			this.name = name;
+			this.strength = strength;
+			this.attack = attack;
+			this.HP = hp;
+			this.defense = defense
+			this.fireWeakness = fireWeakness;
+			this.iceWeakness = iceWeakness;
+			this.lightningWeakness = lightningWeakness;
+			this.burn = false;
+			this.frostbite = false;
+			this.shock = false;
+			this.imageURL = imageURL;
+			this.expPts = exp;
 		}
-	},
-	manaPotion(){
-		if(this.manaPotions > 0 && this.currentMP < this.maxMP){
-			if(Math.ceil(manaPotion.recovery * this.maxMP) + this.currentMP >= this.maxMP){
-				this.currentMP = this.maxMP;
-			} else{
-				this.currentMP += Math.ceil(healthPotion.recovery * this.maxMP);
-			}
-			$('#currentMP').text(player.currentMP);
-			$('#manaBar').css('width', `${(this.currentMP/this.maxMP)*100}%`);
-			this.manaPotions--;
-		} else if(this.manaPotions = 0){
-				$($update).prepend('<p>You have no mana potions left</p>')
-		} else{
-			$($update).prepend('<p>Mana is already at max</p>');
+		attack(){
+			$($update).prepend(`<p>The enemy attacks you!</p>`);
 		}
 	}
-}
+
+	const bat = new enemies('Bat', 2, "bite", 15, 2, false, false, true, 'url(images/enemies/bat.gif)', 2);
+	const snake = new enemies('Snake', 3, "bite", 10, 2, true, true, false, 'url(images/enemies/snake.gif)', 2);
+
+	const cave = [bat];
+	const field = [snake];
+
+
+//Potions
+	const healthPotion = {
+		name: 'Health Potion',
+		recovery: 0.5,
+	};
+
+	const manaPotion = {
+		name: 'Mana Potion',
+		recovery: 0.5,
+	}
+
+//Player
+	const player = {
+		maxHP: 20,
+		maxMP: 8,
+		currentHP: 20,
+		currentMP: 8,
+		level: 1,
+		strength: 6,
+		defense: 3,
+		currentEXP: 0,
+		levelUpEXP: 5,
+		healthPotions: 3,
+		manaPotions: 3,
+		playerTurn(){
+			$($update).prepend('<p>You attack the enemy!</p>');
+		},
+		levelUp(){
+			$($update).prepend('<p>You have leveled up!</p>');
+			player.level++;
+			player.maxHP +=3;
+			player.maxMP +=2;
+			player.currentHP = player.maxHP;
+			player.currentMP = player.maxMP;
+			player.strength++;
+			player.defense++;
+			player.levelUpEXP += Math.floor(5*0.75*player.level);
+			$($update).prepend(`<p>Max HP = ${player.maxHP}</p>`);
+			$($update).prepend(`<p>Max MP = ${player.maxMP}</p>`);
+			$($update).prepend(`<p>Strength = ${player.strength}</p>`);
+			$($update).prepend(`<p>Defense = ${player.defense}</p>`);
+			$($update).prepend(`<p>EXP to next level: ${player.levelUpEXP - player.currentEXP}</p>`);
+		},
+		fireSpell(){
+			let dmg = 5;
+			const burnChance = .25;
+			game.currentEnemy.HP -= dmg;
+			if(Math.random() < burnChance){
+				game.currentEnemy.burn = true;
+				$($update).prepend(`${game.currentEnemy.name} has been burned.`);
+			}
+			$('#attack-animation').css('background-image', 'url(../images/attacks/fireball.gif)');
+		},
+		iceSpell(){
+			let dmg = 5;
+			const frostbiteChance = .25;
+			game.currentEnemy.HP -= dmg;
+			if(Math.random() < frostbiteChance){
+				game.currentEnemy.frostbite = true;
+				$($update).prepend(`${game.currentEnemy.name} has been frostbitten.`);
+			}
+			$('#attack-animation').css('background-image', 'url(../images/attacks/ice.gif)');
+		},
+		lightningSpell(){
+			let dmg = 5;
+			const shockChance = .1;
+			game.currentEnemy.HP -= dmg;
+			if(Math.random() < shockChance){
+				game.currentEnemy.shock = true;
+				$($update).prepend(`${game.currentEnemy.name} is in shock.`);
+			}
+			$('#attack-animation').css('background-image', 'url(../images/attacks/lightning.gif)');
+		},
+		healthPotion(){
+			if(this.healthPotions > 0 && this.currentHP < this.maxHP){
+				if(Math.ceil(healthPotion.recovery * this.maxHP) + this.currentHP >= this.maxHP){
+					this.currentHP = this.maxHP;
+				} else{
+					this.currentHP += Math.ceil(healthPotion.recovery * this.maxHP);
+				}
+				console.log(player.currentHP);
+				$('#currentHP').text(player.currentHP);
+				$('#hpBar').css('width', `${(this.currentHP/this.maxHP)*100}%`);
+				this.healthPotions--;
+			} else{
+				if(this.healthPotions = 0){
+					$($update).prepend('<p>You have no health potions left</p>')
+				} else{
+					$($update).prepend('<p>Health is already at max</p>');
+				}
+			}
+		},
+		manaPotion(){
+			if(this.manaPotions > 0 && this.currentMP < this.maxMP){
+				if(Math.ceil(manaPotion.recovery * this.maxMP) + this.currentMP >= this.maxMP){
+					this.currentMP = this.maxMP;
+				} else{
+					this.currentMP += Math.ceil(healthPotion.recovery * this.maxMP);
+				}
+				$('#currentMP').text(player.currentMP);
+				$('#manaBar').css('width', `${(this.currentMP/this.maxMP)*100}%`);
+				this.manaPotions--;
+			} else if(this.manaPotions = 0){
+					$($update).prepend('<p>You have no mana potions left</p>')
+			} else{
+				$($update).prepend('<p>Mana is already at max</p>');
+			}
+		}
+	}
 
 $($clearLogButton).on('click', (e) => {
 	console.log('clear');
@@ -290,8 +318,36 @@ const game = {
 		burn: null,
 		frostbite: null,
 		shock: null,
-		imageURL: null
+		imageURL: null,
+		exp: null
 	},
 	battleRound: 1,
-	zone: "fields"
+	zone: "fields",
+	selectEnemy(){
+		if(game.zone === 'fields'){
+			const randomIndex = Math.floor(Math.random()*field.length);
+			game.currentEnemy.name = field[randomIndex].name;
+			game.currentEnemy.strength = field[randomIndex].strength;
+			game.currentEnemy.HP = field[randomIndex].HP;
+			game.currentEnemy.fireWeakness = field[randomIndex].fireWeakness;
+			game.currentEnemy.iceWeakness = field[randomIndex].iceWeakness;
+			game.currentEnemy.lightningWeakness = field[randomIndex].lightningWeakness;
+			game.currentEnemy.burn = field[randomIndex].burn;
+			game.currentEnemy.frostbite = field[randomIndex].frostbite;
+			game.currentEnemy.shock = field[randomIndex].shock;
+			game.currentEnemy.imageURL = field[randomIndex].imageURL;
+			game.currentEnemy.exp = field[randomIndex].expPts;
+			$('#enemy-image').css('background-image', game.currentEnemy.imageURL);
+			$('#enemyHealth').append(`<h6>${game.currentEnemy.HP}`);
+			if(game.currentEnemy.fireWeakness === true){
+				$('#enemyWeakness').append('<p style="color: orange">Fire</p>');
+			}
+			if(game.currentEnemy.iceWeakness === true){
+				$('#enemyWeakness').append('<p style="color: teal">Ice</p>');
+			}
+			if(game.currentEnemy.lightningWeakness === true){
+				$('#enemyWeakness').append('<p style="color: Purple">Lightning</p>');
+			}
+		}
+	}
 }
